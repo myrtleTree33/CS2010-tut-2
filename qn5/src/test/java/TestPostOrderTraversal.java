cffe46b62f088156d904b2ff6088311cea5e762e
Joel Tong <me@joeltong.org>
2015-01-25 16:03:42 -0800

    first commit

---
+++ qn5/src/test/java/TestPostOrderTraversal.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author A0108165J
+ *
+ */
+public class TestPostOrderTraversal {
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  /**
+   * Test method for {@link BinaryTree#postorderToString()}.
+   * @throws IOException 
+   */
+  @Test
+  public void testPostorderToString() throws IOException {
+    BufferedReader bR = new BufferedReader(new FileReader("in1.txt"));
+    BinaryTree<String> bt = BinaryTree.readBinaryTree(bR);
+    System.out.println(bt.postorderToString());
+  }
+
+}
